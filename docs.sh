8be3b59e8858273305138796840cd938ddab631b
Roger Padrell <padrell.roger@gmail.com>
2025-01-01 12:26:52 +0100

    Index will not be documentation

--- docs.sh
+++ docs.sh
@@ -6,5 +6,6 @@ nim doc --project --index:on --outdir:docs src/tomato_engine.nim
 nim jsondoc --project --index:on --outdir:docs/json src/tomato_engine.nim
 # Generate docs.md
 deno run --allow-read --allow-write generateReadme.ts
-# Copy docs.md to /docs
-cp docs.md docs/README.md
+# Copy docs.md and README.md to /docs
+cp docs.md docs/docs.md
+cp README.md docs/README.md
